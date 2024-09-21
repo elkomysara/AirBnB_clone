d07b22fbc4a6598b72bd1cecdd67424c86581c28
elkomysara <sa1244@fayoum.edu.eg>
2024-09-21 23:02:27 +0300

    Make code pycodestyle compliant

--- models/base_model.py
+++ models/base_model.py
@@ -1,24 +1,25 @@
 #!/usr/bin/python3
 """Base model module"""
+from datetime import datetime
 
 class BaseModel:
     """Base class for all models"""
 
     def __init__(self):
-        """Initialize the BaseModel"""
+        """Initialize the BaseModel with datetime and unique id"""
         self.id = "1234"
-        self.created_at = "2023-09-21"
-        self.updated_at = "2023-09-21"
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
 
     def save(self):
-        """Simulate saving the model"""
-        self.updated_at = "2023-09-21"
+        """Update the updated_at attribute to current time"""
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         """Returns dictionary representation of instance"""
         return {
             "id": self.id,
-            "created_at": self.created_at,
-            "updated_at": self.updated_at,
+            "created_at": self.created_at.isoformat(),
+            "updated_at": self.updated_at.isoformat(),
             "__class__": "BaseModel"
         }
