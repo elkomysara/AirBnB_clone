613009df7fa5814e0e7204db42fc610f4f959cc8
elkomysara <sa1244@fayoum.edu.eg>
2024-09-22 23:39:18 +0300

    Updated FileStorage and console to support State, City, Amenity, Place, Review

--- console.py
+++ console.py
@@ -2,6 +2,7 @@
 """Entry point of the command interpreter."""
 
 import cmd
+from models import storage
 from models.base_model import BaseModel
 from models.user import User
 from models.state import State
@@ -9,8 +10,6 @@ from models.city import City
 from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
-from models import storage
-
 
 class HBNBCommand(cmd.Cmd):
     """Command interpreter for the HBNB console."""
@@ -28,7 +27,7 @@ class HBNBCommand(cmd.Cmd):
     }
 
     def do_create(self, arg):
-        """Creates a new instance of BaseModel, User, State, City, Amenity, Place, or Review, saves it, and prints the id."""
+        """Creates a new instance of a given class, saves it, and prints the id."""
         if not arg:
             print("** class name missing **")
         elif arg not in self.classes:
@@ -72,18 +71,18 @@ class HBNBCommand(cmd.Cmd):
                 storage.save()
 
     def do_all(self, arg):
-        """Prints all string representation of all instances or based on class name."""
+        """Prints all string representations of all instances or based on class name."""
         if arg and arg not in self.classes:
             print("** class doesn't exist **")
         else:
             obj_list = []
             for obj in storage.all().values():
-                if not arg or obj.__class__.__name__ == arg:
+                if not arg or arg == obj.__class__.__name__:
                     obj_list.append(str(obj))
             print(obj_list)
 
     def do_update(self, arg):
-        """Updates an instance based on the class name and id by adding or updating attribute."""
+        """Updates an instance based on the class name and id."""
         args = arg.split()
         if not args:
             print("** class name missing **")
@@ -105,20 +104,6 @@ class HBNBCommand(cmd.Cmd):
                 attr_value = args[3].strip('"')
                 setattr(obj, attr_name, attr_value)
                 obj.save()
-
-    def do_quit(self, arg):
-        """Quit command to exit the program."""
-        return True
-
-    def do_EOF(self, arg):
-        """EOF command to exit the program."""
-        print()
-        return True
-
-    def emptyline(self):
-        """Do nothing on empty input line."""
-        pass
-
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
