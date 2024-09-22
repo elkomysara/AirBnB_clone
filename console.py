ae34d98fcdfbd87a1effb5b96420184007c18960
elkomysara <sa1244@fayoum.edu.eg>
2024-09-22 21:41:08 +0300

    Fix import path for models in test_save_reload_base_model

--- console.py
+++ console.py
@@ -1,23 +1,106 @@
 #!/usr/bin/python3
-""" Console Module for the AirBnB clone project """
+""" Console Module """
 import cmd
+from models.base_model import BaseModel
+from models import storage
+
 
 class HBNBCommand(cmd.Cmd):
-    """Command interpreter for the AirBnB clone"""
-    prompt = "(hbnb) "  # Set the custom prompt
+    """Command processor class for HBNB"""
+    prompt = "(hbnb) "
 
     def do_quit(self, arg):
         """Quit command to exit the program"""
-        return True  # Returning True exits the cmd loop
+        return True
 
     def do_EOF(self, arg):
         """EOF command to exit the program"""
-        print()  # Print a newline before exiting
-        return True  # Returning True exits the cmd loop
+        print()
+        return True
 
     def emptyline(self):
-        """Do nothing when an empty line is entered"""
-        pass  # Overrides the default behavior which repeats the last command
+        """Do nothing on empty input line"""
+        pass
+
+    def do_create(self, arg):
+        """Creates a new instance of BaseModel, saves it, and prints the id"""
+        if not arg:
+            print("** class name missing **")
+        elif arg != "BaseModel":
+            print("** class doesn't exist **")
+        else:
+            new_instance = BaseModel()
+            new_instance.save()
+            print(new_instance.id)
+
+    def do_show(self, arg):
+        """Prints the string representation of an instance based on class name and id"""
+        args = arg.split()
+        if not args:
+            print("** class name missing **")
+        elif args[0] != "BaseModel":
+            print("** class doesn't exist **")
+        elif len(args) == 1:
+            print("** instance id missing **")
+        else:
+            key = f"{args[0]}.{args[1]}"
+            if key not in storage.all():
+                print("** no instance found **")
+            else:
+                print(storage.all()[key])
+
+    def do_destroy(self, arg):
+        """Deletes an instance based on class name and id"""
+        args = arg.split()
+        if not args:
+            print("** class name missing **")
+        elif args[0] != "BaseModel":
+            print("** class doesn't exist **")
+        elif len(args) == 1:
+            print("** instance id missing **")
+        else:
+            key = f"{args[0]}.{args[1]}"
+            if key not in storage.all():
+                print("** no instance found **")
+            else:
+                del storage.all()[key]
+                storage.save()
+
+    def do_all(self, arg):
+        """Prints all string representation of all instances or based on class name"""
+        if arg and arg != "BaseModel":
+            print("** class doesn't exist **")
+        else:
+            obj_list = []
+            for obj in storage.all().values():
+                obj_list.append(str(obj))
+            print(obj_list)
+
+    def do_update(self, arg):
+        """Updates an instance based on the class name and id"""
+        args = arg.split()
+        if not args:
+            print("** class name missing **")
+        elif args[0] != "BaseModel":
+            print("** class doesn't exist **")
+        elif len(args) == 1:
+            print("** instance id missing **")
+        elif len(args) == 2:
+            print("** attribute name missing **")
+        elif len(args) == 3:
+            print("** value missing **")
+        else:
+            key = f"{args[0]}.{args[1]}"
+            if key not in storage.all():
+                print("** no instance found **")
+            else:
+                obj = storage.all()[key]
+                attr_name = args[2]
+                attr_value = args[3].strip('"')
+                setattr(obj, attr_name, attr_value)
+                obj.save()
+
 
 if __name__ == '__main__':
-    HBNBCommand().cmdloop()  # Start the command loop
+    HBNBCommand().cmdloop()
+
