08fa25aa72303b646d194718552fa2c0fe40eca6
elkomysara <sa1244@fayoum.edu.eg>
2024-09-22 09:35:49 +0300

    Implement BaseModel and initialize models package

--- models/base_model.py
+++ models/base_model.py
@@ -1,25 +1,37 @@
 #!/usr/bin/python3
 """Base model module"""
+
+import uuid
 from datetime import datetime
 
 class BaseModel:
     """Base class for all models"""
 
-    def __init__(self):
-        """Initialize the BaseModel with datetime and unique id"""
-        self.id = "1234"
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+    def __init__(self, *args, **kwargs):
+        """Initialize a new instance of BaseModel"""
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != '__class__':
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+
+    def __str__(self):
+        """Return the string representation of the BaseModel"""
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
-        """Update the updated_at attribute to current time"""
+        """Update the updated_at attribute with the current time"""
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        """Returns dictionary representation of instance"""
-        return {
-            "id": self.id,
-            "created_at": self.created_at.isoformat(),
-            "updated_at": self.updated_at.isoformat(),
-            "__class__": "BaseModel"
-        }
+        """Return a dictionary containing all keys/values of the instance"""
+        dict_repr = self.__dict__.copy()
+        dict_repr['created_at'] = self.created_at.isoformat()
+        dict_repr['updated_at'] = self.updated_at.isoformat()
+        dict_repr['__class__'] = self.__class__.__name__
+        return dict_repr
