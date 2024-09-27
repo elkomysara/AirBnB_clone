959fab94f8c4d387b75240822b5338b752a8a836
elkomysara <sa1244@fayoum.edu.eg>
2024-09-27 13:45:33 +0300

    Fix invocation of all command using onecmd

--- run_commands.py
+++ run_commands.py
@@ -8,11 +8,11 @@ cmd = HBNBCommand()
 cmd.onecmd('create State name="California"')
 cmd.onecmd('create State name="Arizona"')
 
-# Execute 'all State' using onecmd to see all State objects
+# Invoke the 'all' command to print all 'State' instances
 cmd.onecmd('all State')
 
 # Simulate the 'create' command for Place with parameters
 cmd.onecmd('create Place city_id="0001" user_id="0001" name="My_little_house" number_rooms=4 number_bathrooms=2 max_guest=10 price_by_night=300 latitude=37.773972 longitude=-122.431297')
 
-# Execute 'all Place' to see the Place objects
+# Invoke the 'all' command to print all 'Place' instances
 cmd.onecmd('all Place')
