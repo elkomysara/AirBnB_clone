cbdbd0363ad21ce774950fc380e4e63b0972c8ef
elkomysara <sa1244@fayoum.edu.eg>
2024-09-27 16:01:21 +0300

    Add SQLAlchemy models for Place, User, and City relationships

--- models/city.py
+++ models/city.py
@@ -1,10 +1,14 @@
 
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 class City(BaseModel, Base):
-    """Representation of a city"""
+    """Representation of a City"""
     
     __tablename__ = 'cities'
+    
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    
+    places = relationship("Place", cascade="all, delete", backref="city")
--- models/user.py
+++ models/user.py
@@ -1,12 +1,16 @@
 
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 class User(BaseModel, Base):
-    """Representation of a user"""
+    """Representation of a User"""
     
     __tablename__ = 'users'
+    
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
+    
+    places = relationship("Place", cascade="all, delete", backref="user")
