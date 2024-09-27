a14491b16e37f67b6617343ba677d4c47d6b2f6a
elkomysara <sa1244@fayoum.edu.eg>
2024-09-27 13:39:05 +0300

    Fix direct invocation of 'all' command in console

--- run_commands.py
+++ run_commands.py
@@ -3,9 +3,15 @@ from console import HBNBCommand
 # Create an instance of the command interpreter
 cmd = HBNBCommand()
 
-# Simulate input commands
+# Simulate the 'create' commands
 cmd.onecmd('create State name="California"')
 cmd.onecmd('create State name="Arizona"')
-cmd.onecmd('all State')
+
+# Directly invoke the 'all' command for 'State'
+cmd.do_all("State")
+
+# Simulate the 'create' command for Place with parameters
 cmd.onecmd('create Place city_id="0001" user_id="0001" name="My_little_house" number_rooms=4 number_bathrooms=2 max_guest=10 price_by_night=300 latitude=37.773972 longitude=-122.431297')
-cmd.onecmd('all Place')
+
+# Directly invoke the 'all' command for 'Place'
+cmd.do_all("Place")
